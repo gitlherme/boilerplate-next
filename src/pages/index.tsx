0543164242856d981e6fa5631a50d0bd111d449d
gitlherme <dev@guilhermevieira.dev>
2021-10-19 20:34:04 -0300

    add eslint, prettier, husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-
 type Props = {
   title: string
 }
 
 export default function Home({ title = 'Guilherme' }: Props) {
-  return (
-    <h1>Hello, { title }</h1>
-  )
+  return <h1>Hello, {title}</h1>
 }
